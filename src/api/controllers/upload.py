f6a90da672b56f74bd1e7a6c382be050d1c1dac9
Udai Arneja <udai.arneja11ua@gmail.com>
2024-02-29 23:46:48 +0000

    directory upload

--- src/api/controllers/upload.py
+++ src/api/controllers/upload.py
@@ -1,16 +1,15 @@
 
 
-from fastapi import APIRouter, File, UploadFile
+from fastapi import APIRouter, File, Form, UploadFile
+from pydantic import BaseModel
 from controllers.upload_service import PhotoUploadService
 
 router = APIRouter()
 
+
 @router.post("/uploadSingleImage")
 def uploadSingleImage(
-        # destination: str,
-        my_file: UploadFile = File(...)
-        ):
-    PhotoUploadService.process(my_file.filename,my_file)
-    print(my_file.file)
-    print()
-    return
+        folder: str = Form(...),
+        image: UploadFile = File(...)
+    ):
+    PhotoUploadService.process(folder, image)
--- src/api/controllers/upload_service.py
+++ src/api/controllers/upload_service.py
@@ -1,6 +1,6 @@
 
+import os
 from fastapi import File, UploadFile
-
 
 class PhotoUploadService:
 
@@ -8,12 +8,15 @@ class PhotoUploadService:
         pass
 
     def process(
-            file_loc: str,
-            my_file: UploadFile = File(...)
+            folderName: str,
+            image: UploadFile = File(...)
         ):
+        if not os.path.exists(f"/Users/udaiarneja/Github/ai_photography/assets/{folderName}"):
+            print(f"Creating folder for duplicate groupings at: /Users/udaiarneja/Github/ai_photography/assets/{folderName}")
+            os.makedirs(f"/Users/udaiarneja/Github/ai_photography/assets/{folderName}")
         try:
-            with open(f"/Users/udaiarneja/Github/ai_photography/assets/{file_loc}", "wb+") as file_object:
-                file_object.write(my_file.file.read())
+            with open(f"/Users/udaiarneja/Github/ai_photography/assets/{folderName}/{image.filename}", "wb+") as file_object:
+                file_object.write(image.file.read())
         except Exception as error:
             print(error)
             print("error")
