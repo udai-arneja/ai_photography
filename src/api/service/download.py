1f8c78d5d87892034074c6038daf7529124010f0
Udai Arneja <udai.arneja11ua@gmail.com>
2024-03-05 22:13:08 +0000

    update info endpoint

--- src/api/service/download.py
+++ src/api/service/download.py
@@ -3,27 +3,32 @@ import os
 
 class PhotoDownloadService:
 
+    currLoc = "/Users/udaiarneja/Github/ai_photography/assets/"
+
     def __init__(self) -> None:
         pass
 
     def getUserAlbums(self, user: str) -> dict:
         albumData = []
-        currLoc = "/Users/udaiarneja/Github/ai_photography/assets/"
-        directories = os.listdir(currLoc + user)
+        directories = os.listdir(self.currLoc + user)
 
         for albumName in directories:
-            location = currLoc + user + "/" + albumName
-            creationTime = os.stat(location).st_birthtime
-            albumData.append(self.createAlbumData(albumName, creationTime))
+            if(albumName[0] != "."):
+                location = self.currLoc + user + "/" + albumName
+                albumData.append(self.createAlbumData(albumName, location))
 
         return {
             "albumData": albumData,
             "metaData": []
         }
 
-    def createAlbumData(self, albumName, createdTime):
-        readableTime = datetime.utcfromtimestamp(createdTime).strftime('%Y-%m-%d %H:%M:%S')
+    def createAlbumData(self, albumName, location):
+        creationTime = os.stat(location).st_birthtime
+        readableTime = datetime.utcfromtimestamp(creationTime).strftime('%Y-%m-%d %H:%M:%S')
+
+        noOfImages = len(os.listdir(location))
         return {
             "name": albumName,
-            "created": readableTime
+            "created": readableTime,
+            "quanImages":noOfImages
         }
